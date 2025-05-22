5cee613f09cd8589fdace674cc2a9ca75a588630
kienquoc2102 <kienquoc2102bmt@gmail.com>
2025-05-22 13:50:36 +0700

    change API URL

--- src/pages/VideoChat.js
+++ src/pages/VideoChat.js
@@ -20,7 +20,7 @@ export default function VideoChat() {
           return;
         }
 
-        const newSocket = io('https://quick-meeting-server.onrender.com/');
+        const newSocket = io('http://3.106.188.116:5090');
         setSocket(newSocket);
 
         // Initialize local video
--- src/socket.js
+++ src/socket.js
@@ -1,4 +1,4 @@
 // src/socket.js
 import { io } from 'socket.io-client';
-const socket = io('http://localhost:5090');
+const socket = io('http://3.106.188.116:5090');
 export default socket;
