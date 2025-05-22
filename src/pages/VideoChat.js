c23767051801a81a7b76d70fed4fd82333c970ed
kienquoc2102 <kienquoc2102bmt@gmail.com>
2025-05-22 14:13:12 +0700

    chang url from http to https

--- src/pages/VideoChat.js
+++ src/pages/VideoChat.js
@@ -20,7 +20,7 @@ export default function VideoChat() {
           return;
         }
 
-        const newSocket = io('http://3.106.188.116:5090');
+        const newSocket = io('https://3.106.188.116.nip.io');
         setSocket(newSocket);
 
         // Initialize local video
